b39f4c566b6be7c42446ee380794e29405416669
Neji khouja <njkhouja@gmail.com>
2025-06-01 02:31:33 +0100

    added materielle id for feedback table

--- database/migrations/2025_05_23_220202_create_feedbacks_table.php
+++ database/migrations/2025_05_23_220202_create_feedbacks_table.php
@@ -14,9 +14,10 @@ return new class extends Migration
         Schema::create('feedbacks', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
-            $table->foreignId('target_id')->constrained('users')->onDelete('cascade')->nullable();
-            $table->foreignId('event_id')->constrained()->onDelete('cascade')->nullable();
-            $table->foreignId('zone_id')->constrained('camping_zones')->onDelete('cascade')->nullable();
+            $table->foreignId('target_id')->nullable()->constrained('users')->onDelete('cascade');
+            $table->foreignId('event_id')->nullable()->constrained()->onDelete('cascade');
+            $table->foreignId('zone_id')->nullable()->constrained('camping_zones')->onDelete('cascade');
+            $table->foreignId('materielle_id')->nullable()->constrained()->onDelete('cascade');
             $table->string('contenu')->nullable();
             $table->string('response')->nullable();
             $table->float('note')->check('note >= 0');
